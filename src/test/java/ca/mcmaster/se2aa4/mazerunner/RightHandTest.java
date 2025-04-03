e30616ee91df2942f9f9b487277039fe74b8e1d6
t-ayderus <ayderut@mcmaster.ca>
2025-04-03 15:54:12 -0400

    Update RightHandTest.java

--- src/test/java/ca/mcmaster/se2aa4/mazerunner/RightHandTest.java
+++ src/test/java/ca/mcmaster/se2aa4/mazerunner/RightHandTest.java
@@ -15,17 +15,13 @@ public class RightHandTest{
     public void setUp(){
         this.maze = new Maze();
         this.maze.setMaze("src/test/java/ca/mcmaster/se2aa4/mazerunner/dummy.maz.txt");
-        //this.maze.setMaze("./examples/straight.maz.txt");
-        this.observer = new PathObservor();
-        this.rightHand = new RightHand(this.maze);
-        this.rightHand.addObserver(this.observer);
+        this.rightHand = new RightHand(this.maze, this.maze.getEntrance());
     }
     //Test to check the Right Hand Algorithm is working properly
     @Test
     public void testFindPath(){
         this.setUp();
         assertEquals("FFFFFFF", rightHand.findPath());
-        System.setOut(originalOut);
     }
 
 }
